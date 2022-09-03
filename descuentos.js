40b64f789e261a3d519af1708894575f7c2cbdff
Kleber Coro <kgcoro@gmail.com>
2022-09-02 21:44:48 -0500

    codigo descuentos con cupones

--- descuentos.js
+++ descuentos.js
@@ -5,21 +5,44 @@ const pResult = document.querySelector('#result')
 
 btn.addEventListener('click', calDiscountPrice);
 
+const couponList = [];
+
+couponList.push({
+    name: 'Normal',
+    discount: 10,
+});
+couponList.push({
+    name: 'Silver',
+    discount: 20,
+});
+couponList.push({
+    name: 'Gold',
+    discount: 35,
+});
+
 function calDiscountPrice() {
     // (P * (100 - D)) / 100
 
     const price = Number(inputPrice.value);
-    const discount = Number(inputDiscount.value);
+    const coupon = inputDiscount.value;
 
-    if (!price || !discount) {
-        pResult.innerText = 'Por favor llena con datos el dormulario';
+    if (!price || !coupon) {
+        pResult.innerText = 'Por favor llena con datos el formulario';
         return;
     }
 
-    if (discount >= 100) {
-        pResult.innerText = 'No podemos regalarle el producto'
+    const discountValue = couponList.find(couponName => couponName.name == coupon)
+
+    let discount;
+    if (discountValue) {
+        discount = discountValue.discount
     } else {
-        const newPrice = (price * (100 - discount)) / 100;
-        pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
+        pResult.innerText = 'No existe ese cupon';
+        return;
     }
+
+    console.log(discountValue)
+
+    const newPrice = (price * (100 - discount)) / 100;
+    pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
 }
