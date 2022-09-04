4f8a93131ab8b709db84a3352e2be45aa32e07eb
Kleber Coro <kgcoro@gmail.com>
2022-09-04 16:23:20 -0500

    calculo de la mediana

--- platzimath.js
+++ platzimath.js
@@ -5,4 +5,33 @@ function calculoPromedio(list) {
 
     const promedio = sumList / list.length;
     console.log(promedio);
+    return promedio;
+}
+
+function esParImpar(list) {
+    if (list.length % 2)
+        return true; // impar
+    else
+        return false; // par
+}
+
+function mediana(list) {
+    if (esParImpar(list)) {
+        const indexList = Math.floor(list.length / 2);
+        const medianaImpar = list[indexList];
+        console.log({
+            indexList,
+            medianaImpar
+        });
+        return medianaImpar
+    } else {
+        const indexListOne = Math.floor(list.length / 2);
+        const indexListTwo = indexListOne - 1;
+        const medianaPar = calculoPromedio([list[indexListOne], list[indexListTwo]]);
+        console.log({
+            indexListOne,
+            indexListTwo
+        })
+        return medianaPar;
+    }
 }
